b077d04c2a5a02291eba701c6cb4d28dca22cfca
Spakir <danntyl@mail.ru>
2025-04-19 00:19:35 +0300

    Default configuration

---
+++ src/main/java/com/gridnine/testing/flightrules/filter/FlightFilter.java
@@ -0,0 +1,14 @@
+package com.gridnine.testing.flightrules.filter;
+
+import com.gridnine.testing.flightrules.entity.Flight;
+
+import java.util.List;
+
+public class FlightFilter {
+
+    public static List<Flight> filter(List<Flight> flights, FlightRuleFilter flightRule){
+        return flights.stream()
+                .filter(flightRule.negate())
+                .toList();
+    }
+}
