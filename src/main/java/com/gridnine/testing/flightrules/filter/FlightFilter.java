a5d5b9fbc0c3ebfbde4ad8571d9cc776ea94cd68
Spakir <danntyl@mail.ru>
2025-04-19 18:20:25 +0300

    Класс для фильтрации по FlightRuleFilter

--- src/main/java/com/gridnine/testing/flightrules/filter/FlightFilter.java
+++ src/main/java/com/gridnine/testing/flightrules/filter/FlightFilter.java
@@ -8,7 +8,7 @@ public class FlightFilter {
 
     public static List<Flight> filter(List<Flight> flights, FlightRuleFilter flightRule){
         return flights.stream()
-                .filter(flightRule.negate())
+                .filter(flightRule)
                 .toList();
     }
 }
